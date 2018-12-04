f2a38aad193c8839389c2b6ddaca591d93b7ecc5
BrainBran <32146987+BrainBran@users.noreply.github.com>
2018-12-03 20:59:57 -0500

    put contains back to 11, header 8->7

--- cse331.py
+++ cse331.py
@@ -17,8 +17,8 @@ class HTTP_Controller:
 
                 # parts = [HEADER:____,CONTAINS:"______"]
                 if(parts.__len__() == 2):
-                    header = parts[0][8:]
-                    contains = parts[1][10:-1]
+                    header = parts[0][7:]
+                    contains = parts[1][11:-1]
                     user_agents = flow.request.headers[header]
                     if (contains in user_agents):
                         log.write("User-Agent attack")
@@ -29,7 +29,7 @@ class HTTP_Controller:
                     method = flow.request.method
                     request_method = parts[0][15:]
                     parameter = parts[1][11:]
-                    contains = parts[2][10:-1]
+                    contains = parts[2][11:-1]
 
                     if (request_method == "GET"):
                         if (method == "GET"):
